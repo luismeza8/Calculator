37ffcabf2f3ee6e5307f635087608ae7089bb7ba
luismeza8 <luismz@protonmail.com>
2022-12-17 18:53:53 -0700

    4 buttons added

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button Cliked", Toast.LENGTH_LONG).show()
+    }
+
 }
